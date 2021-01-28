00ec3aaa9a30c7fab12d9dedc31e5fefa02cd942
rathiakshay <rathi.akshay.2108@gmail.com>
2021-01-28 23:27:08 +0530

    Completed the navBar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,13 @@ import SearchIcon from '@material-ui/icons/Search'
 import HomeIcon from '@material-ui/icons/Home'
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
 import HeaderOption from '../HeaderOption/HeaderOption'
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
+import ChatIcon from '@material-ui/icons/Chat'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+
+
 function Header() {
+    
     return (
         <div className="header">
             <div className="header__left">
@@ -17,6 +23,10 @@ function Header() {
             <div className="header__right">
                 <HeaderOption Icon={HomeIcon} title="Home" />
                 <HeaderOption Icon={SupervisorAccountIcon} title="MyNetwork" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Message" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <HeaderOption avatar="" title="" />
             </div>
         </div>
     )
