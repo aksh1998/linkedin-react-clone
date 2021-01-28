3e401fabf50f9012790a3c321a8c07ec28a66952
rathiakshay <akshay.rathi@intimetec.com>
2021-01-28 23:01:41 +0530

    added headerOption component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import "./Header.css"
 import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import HeaderOption from '../HeaderOption/HeaderOption'
 function Header() {
-    return (  
+    return (
         <div className="header">
             <div className="header__left">
                 <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="linkedin" />
                 <div className="header__search">
-                    <SearchIcon/>
-                    <input type="text"/>
+                    <SearchIcon />
+                    <input type="text" />
                 </div>
             </div>
-            <div className="header__right"></div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="MyNetwork" />
+            </div>
         </div>
     )
 }
